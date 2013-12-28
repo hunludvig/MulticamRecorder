926fff933b52adec8e9ece399e6161215ce63148
hunludvig <hunludvig@gmail.com>
2013-12-28 16:08:02 +0200

    Create moniker printer application to get moniker string of available cams.

---
+++ MonikerPrinter/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Xml.Linq;
+using AForge.Video.DirectShow;
+
+namespace MulticamRecorder
+{
+
+
+    public class MonikerPrinter
+    {
+        public static void Main()
+        {
+            ICollection filters = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (AForge.Video.DirectShow.FilterInfo filter in filters)
+            {
+                Console.WriteLine(filter.Name + ":");
+                Console.WriteLine(new XText(filter.MonikerString).ToString());
+            }
+            Console.ReadLine();
+        }
+    }
+}
