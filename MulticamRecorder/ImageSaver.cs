3373b78a4fc59ff148cc347e3877ccb7392e384b
hunludvig <hunludvig@gmail.com>
2014-04-03 13:32:20 +0300

    Separate Image saving

---
+++ MulticamRecorder/ImageSaver.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Text;
+using System.Windows.Media.Imaging;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace MulticamRecorder
+{
+    public abstract class ImageSaver
+    {
+        public String Filename { get; set; }
+        
+        protected abstract BitmapEncoder newEncoder();
+
+        public void SaveImage(object sender, ImagingEventArgs args)
+        {
+            Action<object> saveImageAction = (object bm) => SaveImage((ImagingEventArgs)bm);
+            Task.Factory.StartNew(saveImageAction, args, TaskCreationOptions.AttachedToParent);
+        }
+
+        private void SaveImage(ImagingEventArgs image)
+        {
+            BitmapEncoder Encoder = newEncoder();
+            Encoder.Frames.Add(image.Bitmap);
+            String filename = generateFilename(image);
+            Directory.CreateDirectory(Path.GetDirectoryName(filename));
+            FileStream file = new FileStream(filename, FileMode.Create);
+            Encoder.Save(file);
+            file.Close();
+        }
+
+        private String generateFilename(ImagingEventArgs image)
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.AppendFormat(Filename, image.Frame, image.Timestamp);
+            return builder.ToString();
+        }
+    }
+
+    public class PngSaver: ImageSaver {
+        protected override BitmapEncoder newEncoder()
+        {
+            return new PngBitmapEncoder();
+        }
+    }
+
+    public class JpegSaver : ImageSaver
+    {
+        protected override BitmapEncoder newEncoder()
+        {
+            return new JpegBitmapEncoder();
+        }
+    }
+}
--- MulticamRecorder/Program.cs
+++ MulticamRecorder/Program.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Text;
-using System.Windows.Media.Imaging;
-using System.IO;
 using System.Collections;
 using System.Diagnostics;
-using System.Threading.Tasks;
 
 using Spring.Context;
 using Spring.Context.Support;
@@ -16,7 +12,6 @@ namespace MulticamRecorder
     class Program
     {
         static private Stopwatch watch;
-        static ICamera Device;
 
         private static ILog log = LogManager.GetCurrentClassLogger();
 
@@ -62,46 +57,6 @@ namespace MulticamRecorder
         static void ShowFps(object sender, ImagingEventArgs args)
         {
             Console.WriteLine(args.Frame * 1000f / watch.ElapsedMilliseconds);
-        }
-    }
-
-    class PngSaver
-    {
-        public String Filename { get; set; }
-        public String EncoderClass { get; set;}
-            //set {
-            //    _EncoderClass = Type.GetType(value);
-            //    if (_EncoderClass.IsSubclassOf(typeof(BitmapEncoder)))
-            //        EncoderClass = value;
-            //    else
-            //        throw new Exception("Not valid subclass of BitmapEncoder: "+value);
-            //} }
-
-        private Type _EncoderClass;
-
-        public void SaveImage(object sender, ImagingEventArgs args)
-        {
-            Action<object> saveImageAction = (object bm) => SaveImage((ImagingEventArgs)bm);
-            Task.Factory.StartNew(saveImageAction, args, TaskCreationOptions.AttachedToParent);
-        }
-
-        private void SaveImage(ImagingEventArgs image)
-        {
-            //if (_EncoderClass == default(Type))
-            //    return;
-            BitmapEncoder Encoder = new PngBitmapEncoder();
-            Encoder.Frames.Add(image.Bitmap);
-            String filename = generateFilename(image);
-            Directory.CreateDirectory(Path.GetDirectoryName(filename));
-            FileStream file = new FileStream(filename, FileMode.Create);
-            Encoder.Save(file);
-            file.Close();
-        }
-
-        private String generateFilename(ImagingEventArgs image) {
-            StringBuilder builder = new StringBuilder();
-            builder.AppendFormat(Filename, image.Frame, image.Timestamp);
-            return builder.ToString();
         }
     }
 }
