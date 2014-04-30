8ca785ab64c99cb5dbc5a9233c0255ca127f4e58
hunludvig <hunludvig@gmail.com>
2014-04-30 12:02:02 +0300

    Remove Aforge depency and depend on MulticamLib

--- MonikerPrinter/Program.cs
+++ MonikerPrinter/Program.cs
@@ -1,23 +1,20 @@
 ﻿using System;
-using System.Collections;
 using System.Xml.Linq;
-using AForge.Video.DirectShow;
+using CatenaLogic;
 
 namespace MulticamRecorder
 {
-
-
     public class MonikerPrinter
     {
         public static void Main()
         {
-            ICollection filters = new FilterInfoCollection(FilterCategory.VideoInputDevice);
-            foreach (AForge.Video.DirectShow.FilterInfo filter in filters)
+            FilterInfo[] filters = CapDevice.DeviceMonikers;
+            foreach (FilterInfo filter in filters)
             {
                 Console.WriteLine(filter.Name + ":");
                 Console.WriteLine(new XText(filter.MonikerString).ToString());
             }
-            Console.ReadLine();
+            ConsoleHelper.waitForKey();
         }
     }
 }
