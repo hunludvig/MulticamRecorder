3552610beee2be9c241342ba39586939c40858dd
hunludvig <hunludvig@gmail.com>
2014-04-30 12:40:24 +0300

    Graphic user interface for Multicam

---
+++ MulticamGui/Recorder.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MulticamGui
+{
+    /// <summary>
+    /// Interaction logic for Recorder.xaml
+    /// </summary>
+    public partial class Recorder : System.Windows.Controls.UserControl
+    {
+        public Recorder()
+        {
+            InitializeComponent();
+        }
+
+        private void BrowsePath(object sender, RoutedEventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.ShowDialog();
+            
+        }
+
+        public void Start()
+        {
+
+        }
+
+        public void Stop()
+        {
+
+        }
+    }
+}
